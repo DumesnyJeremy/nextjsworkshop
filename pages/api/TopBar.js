7b38cef49f9216ffbb3d1ea255799a171960d880
Jeremy Dumesny <jd.st@titandc.fr>
2021-12-15 20:30:58 +0100

    Create TopBar.

---
+++ pages/api/TopBar.js
@@ -0,0 +1,9 @@
+import styles from "../styles/TopBar.modules.css;"
+
+function TopBar () {
+    return (
+        <div>
+            <h2 className={styles.TopBar}> My cool blog </h2>
+        </div>
+    )
+}
